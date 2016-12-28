1d5b7e81483e338f215898d56971dda13aa3b25b
Ben <benoit.hagenbourger@gmail.com>
2016-12-28 21:32:14 +0100

    Release 0.0.2

--- dist/simplecountdown-theme-year-0.0.2.js
+++ dist/simplecountdown-theme-year-0.0.2.js
@@ -9,9 +9,9 @@ SimpleCountdown.addTheme(
             "font-family: sans-serif, 'Arial', 'Helvetica';" +
             "background: #131313;" +
             "background: -moz-linear-gradient(top,  #131313 0%, #2c2c2c 86%, #2c2c2c 86%, #474747 100%, #595959 100%, #666666 100%, #000000 100%, #000000 100%, #111111 100%, #000000 100%, #474747 100%, #1c1c1c 100%, #2b2b2b 100%, #4c4c4c 100%, #111111 100%, #111111 101%);" +
-            "background: -webkit-linear-gradient(top,  #131313 0%,#2c2c2c 86%,#2c2c2c 86%,#474747 100%,#595959 100%,#666666 100%,#000000 100%,#000000 100%,#111111 100%,#000000 100%,#474747 100%,#1c1c1c 100%,#2b2b2b 100%,#4c4c4c 100%,#111111 100%,#111111 101%);" + 
+            "background: -webkit-linear-gradient(top,  #131313 0%,#2c2c2c 86%,#2c2c2c 86%,#474747 100%,#595959 100%,#666666 100%,#000000 100%,#000000 100%,#111111 100%,#000000 100%,#474747 100%,#1c1c1c 100%,#2b2b2b 100%,#4c4c4c 100%,#111111 100%,#111111 101%);" +
             "background: linear-gradient(to bottom,  #131313 0%,#2c2c2c 86%,#2c2c2c 86%,#474747 100%,#595959 100%,#666666 100%,#000000 100%,#000000 100%,#111111 100%,#000000 100%,#474747 100%,#1c1c1c 100%,#2b2b2b 100%,#4c4c4c 100%,#111111 100%,#111111 101%);" +
-            "filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#131313', endColorstr='#111111',GradientType=0 );" + 
+            "filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#131313', endColorstr='#111111',GradientType=0 );" +
             "}",
          title: ".sc-title{" +
             "font-weight: 900;" +
@@ -112,6 +112,60 @@ SimpleCountdown.addTheme(
 
  function callback() {
 
+     //create bottle
+     var divBottle = document.createElement('div');
+     divBottle.className = 'bottle';
+     if (document.body)
+       document.body.insertBefore(divBottle, document.body.firstChild);
+     else
+       document.body.appendChild(divBottle);
+
+      divBottle.style.width = "140px";
+      divBottle.style.height = "15px";
+      divBottle.style.position = "fixed";
+      divBottle.style.opacity = 1;
+      divBottle.style.top = "90.5%";
+      divBottle.style.left = "50%";
+      divBottle.style.marginLeft = "-70px";
+      divBottle.style.backgroundColor = "#558B2F";
+      divBottle.style['border-radius'] = "5%";
+
+      //create bottle
+      var divBottleBase = document.createElement('div');
+      divBottleBase.className = 'bottleBottom';
+      if (document.body)
+        document.body.insertBefore(divBottleBase, document.body.firstChild);
+      else
+        document.body.appendChild(divBottleBase);
+
+       divBottleBase.style.width = "140px";
+       divBottleBase.style.height = "25px";
+       divBottleBase.style.position = "fixed";
+       divBottleBase.style.opacity = 1;
+       divBottleBase.style.top = "92%";
+       divBottleBase.style.left = "50%";
+       divBottleBase.style.marginLeft = "-70px";
+       divBottleBase.style.backgroundColor = "#FFF9C4";
+       divBottleBase.style['border-radius'] = "10%";
+
+       //create bottle
+       var divBottleBig = document.createElement('div');
+       divBottleBig.className = 'bottleBig';
+       if (document.body)
+         document.body.insertBefore(divBottleBig, document.body.firstChild);
+       else
+         document.body.appendChild(divBottleBig);
+
+        divBottleBig.style.width = "140px";
+        divBottleBig.style.height = "40px";
+        divBottleBig.style.position = "fixed";
+        divBottleBig.style.opacity = 1;
+        divBottleBig.style.top = "95%";
+        divBottleBig.style.left = "50%";
+        divBottleBig.style.marginLeft = "-70px";
+        divBottleBig.style.backgroundColor = "#558B2F";
+        divBottleBig.style['border-radius'] = "10%";
+
      function createSnowflake() {
          var div = document.createElement('div');
          div.className = 'snowflake';
@@ -120,14 +174,19 @@ SimpleCountdown.addTheme(
          else
            document.body.appendChild(div);
 
+        //variable random color
+        var color = '#'; // hexadecimal starting symbol
+        var letters = ['FFFDE7', 'FFF59D', 'FDD835', 'FFFF00', 'FFFF8D', 'FFEA00', 'FFD600']; //Set your colors here
+        color += letters[Math.floor(Math.random() * letters.length)];
+
        div.style.width = div.style.height = Math.floor((Math.random() * 15) + 10) + "px";
        div.style.position = "fixed";
-       div.style.backgroundColor = "white";
+       div.style.backgroundColor = color;
        div.style['border-radius'] = "50%";
        div.style.opacity = 0;
        //- positions
-       div.style.left = Math.floor(window.screen.width / 2) + 1 + "px";
-       div.style.top = "100%";
+       div.style.left = Math.floor((window.screen.width - 50) / 2) + "px";
+       div.style.top = "90%";
        return div;
      }
 
@@ -166,8 +225,4 @@ SimpleCountdown.addTheme(
             // console.log(document.getElementsByClassName('snowflake').length);
         }, 500);
     };
-  },
-  function callbackZero(){
-    console.log("mouette");
-  }
-);
+});
